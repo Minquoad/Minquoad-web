49591d3b5e950a00a734824446efbaf898549555
minqu <minqu@DESKTOP-G0TG060>
2019-02-20 22:37:57 +0100

    Gestion du proejt

--- DatabaseCreation.sql
+++ DatabaseCreation.sql
@@ -2,10 +2,10 @@
 -- PostgreSQL database dump
 --
 
--- Dumped from database version 11.0
--- Dumped by pg_dump version 11.0
+-- Dumped from database version 11.1
+-- Dumped by pg_dump version 11.1
 
--- Started on 2018-11-10 12:41:28
+-- Started on 2019-02-20 21:14:06
 
 SET statement_timeout = 0;
 SET lock_timeout = 0;
@@ -17,16 +17,15 @@ SET check_function_bodies = false;
 SET client_min_messages = warning;
 SET row_security = off;
 
-DROP DATABASE "Minquoad-DB";
 --
--- TOC entry 2830 (class 1262 OID 16393)
+-- TOC entry 2180 (class 1262 OID 16384)
 -- Name: Minquoad-DB; Type: DATABASE; Schema: -; Owner: postgres
 --
 
 CREATE DATABASE "Minquoad-DB" WITH TEMPLATE = template0 ENCODING = 'UTF8' LC_COLLATE = 'English_United States.1252' LC_CTYPE = 'English_United States.1252';
 
 
-ALTER DATABASE "Minquoad-DB" OWNER TO "postgres";
+ALTER DATABASE "Minquoad-DB" OWNER TO postgres;
 
 \connect -reuse-previous=on "dbname='Minquoad-DB'"
 
@@ -45,25 +44,25 @@ SET default_tablespace = '';
 SET default_with_oids = false;
 
 --
--- TOC entry 197 (class 1259 OID 24618)
+-- TOC entry 196 (class 1259 OID 16385)
 -- Name: Thing; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE "public"."Thing" (
-    "id" integer NOT NULL,
-    "description" "text",
-    "owner" integer
+CREATE TABLE public."Thing" (
+    id integer NOT NULL,
+    description text,
+    owner integer
 );
 
 
-ALTER TABLE "public"."Thing" OWNER TO "postgres";
+ALTER TABLE public."Thing" OWNER TO postgres;
 
 --
--- TOC entry 196 (class 1259 OID 24616)
+-- TOC entry 197 (class 1259 OID 16391)
 -- Name: Thing_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE "public"."Thing_id_seq"
+CREATE SEQUENCE public."Thing_id_seq"
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -72,41 +71,42 @@ CREATE SEQUENCE "public"."Thing_id_seq"
     CACHE 1;
 
 
-ALTER TABLE "public"."Thing_id_seq" OWNER TO "postgres";
+ALTER TABLE public."Thing_id_seq" OWNER TO postgres;
 
 --
--- TOC entry 2831 (class 0 OID 0)
--- Dependencies: 196
+-- TOC entry 2181 (class 0 OID 0)
+-- Dependencies: 197
 -- Name: Thing_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE "public"."Thing_id_seq" OWNED BY "public"."Thing"."id";
+ALTER SEQUENCE public."Thing_id_seq" OWNED BY public."Thing".id;
 
 
 --
--- TOC entry 199 (class 1259 OID 24633)
+-- TOC entry 198 (class 1259 OID 16393)
 -- Name: User; Type: TABLE; Schema: public; Owner: postgres
 --
 
-CREATE TABLE "public"."User" (
-    "id" integer NOT NULL,
-    "nickname" "text" NOT NULL,
-    "hashedSaltedPassword" "text",
+CREATE TABLE public."User" (
+    id integer NOT NULL,
+    nickname text NOT NULL,
+    "hashedSaltedPassword" text,
     "adminLevel" integer,
     "lastActivityDate" timestamp with time zone,
     "registrationDate" timestamp with time zone,
-    "pictureName" "text"
+    "pictureName" text,
+    "unblockDate" time with time zone
 );
 
 
-ALTER TABLE "public"."User" OWNER TO "postgres";
+ALTER TABLE public."User" OWNER TO postgres;
 
 --
--- TOC entry 198 (class 1259 OID 24631)
+-- TOC entry 199 (class 1259 OID 16399)
 -- Name: User_id_seq; Type: SEQUENCE; Schema: public; Owner: postgres
 --
 
-CREATE SEQUENCE "public"."User_id_seq"
+CREATE SEQUENCE public."User_id_seq"
     AS integer
     START WITH 1
     INCREMENT BY 1
@@ -115,110 +115,61 @@ CREATE SEQUENCE "public"."User_id_seq"
     CACHE 1;
 
 
-ALTER TABLE "public"."User_id_seq" OWNER TO "postgres";
+ALTER TABLE public."User_id_seq" OWNER TO postgres;
 
 --
--- TOC entry 2832 (class 0 OID 0)
--- Dependencies: 198
+-- TOC entry 2182 (class 0 OID 0)
+-- Dependencies: 199
 -- Name: User_id_seq; Type: SEQUENCE OWNED BY; Schema: public; Owner: postgres
 --
 
-ALTER SEQUENCE "public"."User_id_seq" OWNED BY "public"."User"."id";
+ALTER SEQUENCE public."User_id_seq" OWNED BY public."User".id;
 
 
 --
--- TOC entry 2693 (class 2604 OID 24621)
+-- TOC entry 2047 (class 2604 OID 16401)
 -- Name: Thing id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "public"."Thing" ALTER COLUMN "id" SET DEFAULT "nextval"('"public"."Thing_id_seq"'::"regclass");
+ALTER TABLE ONLY public."Thing" ALTER COLUMN id SET DEFAULT nextval('public."Thing_id_seq"'::regclass);
 
 
 --
--- TOC entry 2694 (class 2604 OID 24636)
+-- TOC entry 2048 (class 2604 OID 16402)
 -- Name: User id; Type: DEFAULT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "public"."User" ALTER COLUMN "id" SET DEFAULT "nextval"('"public"."User_id_seq"'::"regclass");
+ALTER TABLE ONLY public."User" ALTER COLUMN id SET DEFAULT nextval('public."User_id_seq"'::regclass);
 
 
 --
--- TOC entry 2822 (class 0 OID 24618)
--- Dependencies: 197
--- Data for Name: Thing; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (103, 'vcwxwxvcwxcvwxc', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (105, 'vdvcwvcxvcxw', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (99, 'zggsfsgdfgfsdgsd', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (106, 'nbcnbccn', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (104, 'fdqdsfqsd', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (100, 'kjhgjhkg', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (101, 'zearfdqsdsqfdsfqdsfq', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (102, 'éééééôôôôôô', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (98, 'à admin mtn', 1);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (107, 'yttreert', NULL);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (90, 'à admin', 1);
-INSERT INTO "public"."Thing" ("id", "description", "owner") VALUES (89, 'à admin', 1);
-
-
---
--- TOC entry 2824 (class 0 OID 24633)
--- Dependencies: 199
--- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
---
-
-INSERT INTO "public"."User" ("id", "nickname", "hashedSaltedPassword", "adminLevel", "lastActivityDate", "registrationDate", "pictureName") VALUES (1, 'Dauchi', 'd164ce7285787838f8aed9b181fbd034115c43bf66b20b256e1ed5a7814a403d', 0, '2018-10-30 22:41:17.766+01', '2018-10-28 19:12:03.669+01', NULL);
-INSERT INTO "public"."User" ("id", "nickname", "hashedSaltedPassword", "adminLevel", "lastActivityDate", "registrationDate", "pictureName") VALUES (0, 'Adminquo', '30d6294cf8f5c9ec336827b7dbd7a04bb36330f36641700b5fd1c173fd91c17c', 2147483647, '2018-11-10 12:16:10.044+01', '2018-10-30 22:00:47.192+01', '1207983481536694400');
-INSERT INTO "public"."User" ("id", "nickname", "hashedSaltedPassword", "adminLevel", "lastActivityDate", "registrationDate", "pictureName") VALUES (7, 'Adminquo554', '53f69dad9ae5547b4e7e6beed49879949584dab5175c0b54643c66f4737c98aa', 0, '2018-11-06 21:46:15.548+01', '2018-11-06 21:46:09.925+01', NULL);
-
-
---
--- TOC entry 2833 (class 0 OID 0)
--- Dependencies: 196
--- Name: Thing_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('"public"."Thing_id_seq"', 107, true);
-
-
---
--- TOC entry 2834 (class 0 OID 0)
--- Dependencies: 198
--- Name: User_id_seq; Type: SEQUENCE SET; Schema: public; Owner: postgres
---
-
-SELECT pg_catalog.setval('"public"."User_id_seq"', 7, true);
-
-
---
--- TOC entry 2696 (class 2606 OID 24626)
+-- TOC entry 2050 (class 2606 OID 16404)
 -- Name: Thing Thing_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "public"."Thing"
-    ADD CONSTRAINT "Thing_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY public."Thing"
+    ADD CONSTRAINT "Thing_pkey" PRIMARY KEY (id);
 
 
 --
--- TOC entry 2698 (class 2606 OID 24641)
+-- TOC entry 2052 (class 2606 OID 16406)
 -- Name: User User_pkey; Type: CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "public"."User"
-    ADD CONSTRAINT "User_pkey" PRIMARY KEY ("id");
+ALTER TABLE ONLY public."User"
+    ADD CONSTRAINT "User_pkey" PRIMARY KEY (id);
 
 
 --
--- TOC entry 2699 (class 2606 OID 24642)
+-- TOC entry 2053 (class 2606 OID 16407)
 -- Name: Thing Thing_owner_fkey; Type: FK CONSTRAINT; Schema: public; Owner: postgres
 --
 
-ALTER TABLE ONLY "public"."Thing"
-    ADD CONSTRAINT "Thing_owner_fkey" FOREIGN KEY ("owner") REFERENCES "public"."User"("id");
+ALTER TABLE ONLY public."Thing"
+    ADD CONSTRAINT "Thing_owner_fkey" FOREIGN KEY (owner) REFERENCES public."User"(id);
 
 
--- Completed on 2018-11-10 12:41:28
+-- Completed on 2019-02-20 21:14:06
 
 --
 -- PostgreSQL database dump complete
